641eff15f33b353ea325b9c4ac28ee287bd1246c
Efe İncir <61871610+ncrefe@users.noreply.github.com>
2023-05-11 00:23:35 +0300

    Update Main.java
    delete

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Muhammed Efe İncir 270201029
-        // Burak Keçeci 290201103
-
         String path = "src/data/users.csv";
 
         // read all users from csv file.
